9ec213d5e95d50998c1f61d5e7d8c991510fe002
Kaylee Knox <kjknoxx90@gmail.com>
2023-10-25 10:44:30 -0500

    Made placeholder Objects for js.

---
+++ js/util.js
@@ -0,0 +1,91 @@
+// First making the objects containing the information for my card projections.
+
+// Object to hold
+let projectInfo = [
+    {
+        project_name: "Code Up Capstone",
+        project_desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut aliquam purus sit amet luctus venenatis lectus. Auctor urna nunc id cursus metus aliquam eleifend. Ac felis donec et odio. Tristique et egestas quis ipsum suspendisse ultrices. Pretium vulputate sapien nec sagittis aliquam. Mattis ullamcorper velit sed ullamcorper.",
+        project_img: "imgs/temps-3.jpg"
+    },
+    {
+        project_name: "Personal Website",
+        project_desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non pulvinar neque. Nunc mattis enim ut tellus elementum sagittis. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Purus non enim praesent elementum facilisis leo vel fringilla. Ac turpis egestas sed tempus urna et pharetra pharetra. Sem et tortor consequat id porta nibh venenatis cras sed.",
+        project_img: "imgs/temps-3.jpg"
+    }
+]
+
+let uiGalleryInfo = [
+    {
+        name: "Personal Website Design",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut aliquam purus sit amet luctus venenatis lectus. Auctor urna nunc id cursus metus aliquam eleifend. Ac felis donec et odio. Tristique et egestas quis ipsum suspendisse ultrices. Pretium vulputate sapien nec sagittis aliquam. Mattis ullamcorper velit sed ullamcorper",
+        img: "imgs/temps-3.jpg"
+    },
+    {
+        name: "Teamfight Tactics UI/UX Re-design",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non pulvinar neque. Nunc mattis enim ut tellus elementum sagittis. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Purus non enim praesent elementum facilisis leo vel fringilla. Ac turpis egestas sed tempus urna et pharetra pharetra. Sem et tortor consequat id porta nibh venenatis cras sed.",
+        img: "imgs/temps-3.jpg"
+    }
+]
+
+let imgGalleryInfo = [
+    {
+        img: "imgs/temps-3.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut. Odio euismod lacinia at quis risus sed vulputate.",
+        title: "Piece 1"
+    },
+    {
+        img: "imgs/temps-3.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut. Odio euismod lacinia at quis risus sed vulputate.",
+        title: "Piece 2"
+    },
+    {
+        img: "imgs/temps-3.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut. Odio euismod lacinia at quis risus sed vulputate.",
+        title: "Piece 3"
+    },
+    {
+        img: "imgs/temps-3.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut. Odio euismod lacinia at quis risus sed vulputate.",
+        title: "Piece 4"
+    },
+    {
+        img: "imgs/temps-3.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut. Odio euismod lacinia at quis risus sed vulputate.",
+        title: "Piece 5"
+    },
+    {
+        img: "imgs/temps-3.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut. Odio euismod lacinia at quis risus sed vulputate.",
+        title: "Piece 6"
+    },
+    {
+        img: "imgs/temps-3.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut. Odio euismod lacinia at quis risus sed vulputate.",
+        title: "Piece 7"
+    },
+    {
+        img: "imgs/temps-3.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut. Odio euismod lacinia at quis risus sed vulputate.",
+        title: "Piece 8"
+    },
+    {
+        img: "imgs/temps-3.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut. Odio euismod lacinia at quis risus sed vulputate.",
+        title: "Piece 9"
+    },
+    {
+        img: "imgs/temps-3.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut. Odio euismod lacinia at quis risus sed vulputate.",
+        title: "Piece 10"
+    },
+    {
+        img: "imgs/temps-3.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut. Odio euismod lacinia at quis risus sed vulputate.",
+        title: "Piece 11"
+    },
+    {
+        img: "imgs/temps-3.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut. Odio euismod lacinia at quis risus sed vulputate.",
+        title: "Piece 12"
+    }
+]
